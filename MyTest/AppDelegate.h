40a4609511aad7d2529182a0ea684eb0fc6ef9f3
hexi <hexi@21tb.com>
2016-04-03 11:33:34 +0800

    first commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by 何溪 on 16/4/3.
+//  Copyright © 2016年 何溪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
